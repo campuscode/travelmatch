f841b76ed7ae41222643547e41b8f424313f9a98
Eduardo e Thais <thais.kusuki@gmail.com>
2018-03-23 22:11:29 -0300

    Mudando rota de busca

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,20 @@
+class SearchController < ApplicationController
+  def index
+    location = params[:location]
+    start_date = params[:start_date]
+    end_date = params[:end_date]
+
+    start_date = Date.parse(params[:start_date]) if start_date.present?
+    end_date = Date.parse(params[:end_date]) if end_date.present?
+
+    # if start_date > end_date
+      # flash[:errors] = 'A data inicial não pode ser maior que a data final'
+    # else
+      @itineraries = Itinerary.where("
+        (location like :location and ((start_date <= :start and end_date >= :start)
+        or (start_date <= :end and end_date >= :end)))",
+        { location: location, start: start_date, end: end_date})
+    # end
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'home#index'
-  get '/search', to: 'home#index'
+  get '/search', to: 'search#index'
   resources :trip_plans, only: [:index,:show,:new,:create] do
   end
 
