c1c0c9525e5ac2c6b37e9c010b0ff776ac768547
Thais Kusuki <thais.kusuki@gmail.com>
2018-03-25 23:30:49 -0300

    Corrigindo os erros gerados com mudança de rota

--- spec/features/traveler_search_trip_plan_spec.rb
+++ spec/features/traveler_search_trip_plan_spec.rb
@@ -4,11 +4,11 @@ feature 'Traveler search for a trip plan' do
   scenario 'successfully' do
 
     trip_plan = TripPlan.create(title: 'Mochilão América do Sul',
-                start_date: '01/01/2019', end_date: '01/02/2019')
+        start_date: '01/01/2019', end_date: '01/02/2019')
 
-    itinerary = Itinerary.create(location: 'São Paulo', season: 'Começo do ano',
-                start_date: '01/01/2019', end_date: '01/02/2019',
-                trip_plan: trip_plan)
+    itinerary = Itinerary.create(location: 'São Paulo',
+        season: 'Começo do ano', start_date: '01/01/2019',
+        end_date: '01/02/2019', trip_plan: trip_plan)
 
     visit root_path
     fill_in 'Localização', with: 'São Paulo'
@@ -27,18 +27,18 @@ feature 'Traveler search for a trip plan' do
   scenario 'and view only the correct trip plan' do
 
     trip_plan = TripPlan.create(title: 'Mochilão América do Sul',
-                start_date: '01/01/2019', end_date: '01/02/2019')
+        start_date: '01/01/2019', end_date: '01/02/2019')
 
-    itinerary = Itinerary.create(location: 'São Paulo', season: 'Começo do ano',
-                start_date: '01/01/2019', end_date: '01/02/2019',
-                trip_plan: trip_plan)
+    itinerary = Itinerary.create(location: 'São Paulo',
+        season: 'Começo do ano', start_date: '01/01/2019',
+        end_date: '01/02/2019', trip_plan: trip_plan)
 
     another_trip_plan = TripPlan.create(title: 'Férias em São Paulo',
-                start_date: '01/03/2019', end_date: '01/04/2019')
+        start_date: '01/03/2019', end_date: '01/04/2019')
 
-    another_itinerary = Itinerary.create(location: 'São Paulo', season: 'Férias escolares',
-                start_date: '01/03/2019', end_date: '01/04/2019',
-                trip_plan: another_trip_plan)
+    another_itinerary = Itinerary.create(location: 'São Paulo',
+        season: 'Férias escolares', start_date: '01/03/2019',
+        end_date: '01/04/2019', trip_plan: another_trip_plan)
 
     visit root_path
     fill_in 'Localização', with: 'São Paulo'
@@ -56,28 +56,28 @@ feature 'Traveler search for a trip plan' do
     expect(page).not_to have_css('li', text: 'Data final: 01/04/2019')
   end
 
-  scenario 'and view two plans' do
+  scenario 'and view results that are in range by start_date' do
 
     trip_plan_one = TripPlan.create(title: 'Mochilão América do Sul',
-                start_date: '01/01/2019', end_date: '01/02/2019')
+        start_date: '01/01/2019', end_date: '01/02/2019')
 
-    itinerary_one = Itinerary.create(location: 'São Paulo', season: 'Começo do ano',
-                start_date: '01/01/2019', end_date: '01/02/2019',
-                trip_plan: trip_plan_one)
+    itinerary_one = Itinerary.create(location: 'São Paulo',
+        season: 'Começo do ano', start_date: '01/01/2019',
+        end_date: '01/02/2019', trip_plan: trip_plan_one)
 
     trip_plan_two = TripPlan.create(title: 'Férias em São Paulo',
-                start_date: '01/03/2019', end_date: '01/04/2019')
+        start_date: '01/03/2019', end_date: '01/04/2019')
 
-    itinerary_two = Itinerary.create(location: 'São Paulo', season: 'Férias escolares',
-                start_date: '01/03/2019', end_date: '01/04/2019',
-                trip_plan: trip_plan_two)
+    itinerary_two = Itinerary.create(location: 'São Paulo',
+        season: 'Férias escolares', start_date: '01/03/2019',
+        end_date: '01/04/2019', trip_plan: trip_plan_two)
 
     trip_plan_three = TripPlan.create(title: 'Mochilão em São Paulo',
-                start_date: '10/01/2019', end_date: '10/03/2019')
+        start_date: '10/01/2019', end_date: '10/03/2019')
 
-    itinerary_three = Itinerary.create(location: 'São Paulo', season: 'Começo do ano',
-                start_date: '10/01/2019', end_date: '10/03/2019',
-                trip_plan: trip_plan_three)
+    itinerary_three = Itinerary.create(location: 'São Paulo',
+        season: 'Começo do ano', start_date: '10/01/2019',
+        end_date: '10/03/2019', trip_plan: trip_plan_three)
 
     visit root_path
     fill_in 'Localização', with: 'São Paulo'
@@ -99,21 +99,21 @@ feature 'Traveler search for a trip plan' do
     expect(page).not_to have_css('li', text: 'Data final: 01/04/2019')
   end
 
-  scenario 'and only end_date in a range results' do
+  scenario 'and view results that are in range by end_date' do
 
     trip_plan_one = TripPlan.create(title: 'Mochilão América do Sul',
-                start_date: '01/01/2019', end_date: '01/02/2019')
+        start_date: '01/01/2019', end_date: '01/02/2019')
 
-    itinerary_one = Itinerary.create(location: 'São Paulo', season: 'Começo do ano',
-                start_date: '01/01/2019', end_date: '01/02/2019',
-                trip_plan: trip_plan_one)
+    itinerary_one = Itinerary.create(location: 'São Paulo',
+        season: 'Começo do ano', start_date: '01/01/2019',
+        end_date: '01/02/2019', trip_plan: trip_plan_one)
 
     trip_plan_two = TripPlan.create(title: 'Férias em São Paulo',
-                start_date: '01/03/2019', end_date: '01/04/2019')
+        start_date: '01/03/2019', end_date: '01/04/2019')
 
-    itinerary_two = Itinerary.create(location: 'São Paulo', season: 'Férias escolares',
-                start_date: '01/03/2019', end_date: '01/04/2019',
-                trip_plan: trip_plan_two)
+    itinerary_two = Itinerary.create(location: 'São Paulo',
+        season: 'Férias escolares', start_date: '01/03/2019',
+        end_date: '01/04/2019', trip_plan: trip_plan_two)
 
     visit root_path
     fill_in 'Localização', with: 'São Paulo'
@@ -131,14 +131,14 @@ feature 'Traveler search for a trip plan' do
     expect(page).not_to have_css('li', text: 'Data final: 01/04/2019')
 
   end
-  scenario 'and dont found results' do
+  scenario 'and dont found any results' do
 
     trip_plan = TripPlan.create(title: 'Mochilão América do Sul',
-                start_date: '01/01/2019', end_date: '01/02/2019')
+        start_date: '01/01/2019', end_date: '01/02/2019')
 
-    itinerary = Itinerary.create(location: 'São Paulo', season: 'Começo do ano',
-                start_date: '01/01/2019', end_date: '01/02/2019',
-                trip_plan: trip_plan)
+    itinerary = Itinerary.create(location: 'São Paulo',
+        season: 'Começo do ano', start_date: '01/01/2019',
+        end_date: '01/02/2019', trip_plan: trip_plan)
 
     visit root_path
     fill_in 'Localização', with: 'São Paulo'
@@ -152,7 +152,7 @@ feature 'Traveler search for a trip plan' do
     expect(page).not_to have_css('li', text: 'Data inicial: 01/01/2019')
     expect(page).not_to have_css('li', text: 'Data final: 01/02/2019')
   end
-  scenario 'Dont acept start_date bigger than end_date' do
+  scenario 'and the start_date cant be bigger than end_date' do
 
     visit root_path
     fill_in 'Localização', with: 'São Paulo'
@@ -160,9 +160,7 @@ feature 'Traveler search for a trip plan' do
     fill_in 'Data final', with: '01/02/2018'
     click_on 'Buscar'
 
-    expect(page).to have_css('p',
-       text: 'A data inicial não pode ser maior que a data final')
-
-
+    expect(page).to have_content('A data inicial não pode ser maior
+                                  que a data final')
   end
 end
